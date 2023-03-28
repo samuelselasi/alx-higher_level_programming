5a70ea29a446b37156dca59dc6da83237d99156c
samuelselasi <samelsekasi@gmail.com>
2023-03-28 06:26:31 -0700

    Add a class Square that defines a square

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# 2-square.py
+"""
+2. Size validation
+A class Square that defines a square
+"""
+
+
+class Square:
+    """Class that defines a square"""
+
+    def __init__(self, size=0):
+        """
+        Define private instance attribute: size
+        Raise TypeError & ValueError if not int or <0 resp.
+        """
+
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
