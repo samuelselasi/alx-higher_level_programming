e55688117a6e13af89c4d0475ee08b5010033bed
samuelselasi <samelsekasi@gmail.com>
2023-03-28 15:31:31 -0700

    Add class MagicClass that does exactly the same as the Python bytecode

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+10. ByteCode -> Python #5
+"""
+import math
+
+
+class MagicClass:
+    """Class that defines a circle"""
+
+    def __init__(self, radius):
+        """
+        Define private instance attribute: radius
+        Raise TypeError if radius is not an integer
+        """
+
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError('radius must be a number')
+
+        self.__radius = radius
+
+    def area(self):
+        """Function that calculates area of a circle"""
+
+        return 2 ** self.__radius * math.pi
+
+    def circumference(self):
+        """Function that calculates circumference of a circle"""
+
+        return 2 * math.pi * self.__radius
