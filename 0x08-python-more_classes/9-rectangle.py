7fb84c6ce1fea181c7074a22e086a2a3fa90774f
samuelselasi <samelsekasi@gmail.com>
2023-04-03 12:21:56 -0700

    Add class Rectangle that defines a rectangle

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -11,30 +11,9 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         """Function that initializes instances for width & height"""
 
-        self.height = height
         self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
-
-    @property
-    def height(self):
-        """Function that retrieves height instance attribute"""
-
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """
-        function that sets height instance attribute
-        Raise TypeError and ValueError if not integer or natural number resp.
-        """
-
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-
-        if value < 0:
-            raise ValueError("height must be >= 0")
-
-        self.__height = value
 
     @property
     def width(self):
@@ -56,6 +35,27 @@ class Rectangle:
             raise ValueError("width must be >= 0")
 
         self.__width = value
+
+    @property
+    def height(self):
+        """Function that retrieves height instance attribute"""
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        function that sets height instance attribute
+        Raise TypeError and ValueError if not integer or natural number resp.
+        """
+
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__height = value
 
     def area(self):
         """Function that computes area of a rectangle"""
