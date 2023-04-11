c97833d49ec6dc941823fd9823593b7756d66d5b
samuelselasi <samelsekasi@gmail.com>
2023-04-11 10:15:37 +0000

    Add class BaseGeometry (based on 5-base_geometry.py) and function area that raises an Exception with the message area() is not implemented

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""6. Improve Geometry"""
+
+
+class BaseGeometry:
+    """class BaseGeometry (based on 5-base_geometry.py)."""
+
+    def area(self):
+        """Function that computes area (not implemented yet)"""
+
+        raise Exception("area() is not implemented")
