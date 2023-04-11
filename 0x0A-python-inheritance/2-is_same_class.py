819945b8fe0ade3f6703bf5328dab616e8ede784
samuelselasi <samelsekasi@gmail.com>
2023-04-11 09:53:47 +0000

    Add a class MyList that inherits from list and a function that prints the list, but sorted (ascending sort)

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -4,7 +4,7 @@
 
 def is_same_class(obj, a_class):
     """
-    A function that returns True if the object is exactly an 
+    A function that returns True if the object is exactly an
     instance of the specified class ; otherwise False.
     """
 
