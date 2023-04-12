c50a8fb835cac848a9c76cf273148b2cb669abd1
samuelselasi <samelsekasi@gmail.com>
2023-04-12 14:13:57 +0000

    Add script that reads stdin line by line and computes metrics

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""14. Log parsing"""
+
+import sys
+
+
+if __name__ == "__main__":
+
+    def status_codes(size, status_codes):
+        """A function that prints status codes after CTRL + C"""
+
+        print("File size: {}".format(size))
+        for key in sorted(status_codes):
+            print("{}: {}".format(key, status_codes[key]))
+
+    size = 0
+    possible_codes = {}
+    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    i = 0
+
+    try:
+        for line in sys.stdin:
+            if i == 10:
+                status_codes(size, possible_codes)
+                i = 1
+
+            else:
+                i += 1
+
+            line = line.split()
+            try:
+                size += int(line[-1])
+
+            except (IndexError, ValueError):
+                pass
+
+            try:
+                if line[-2] in valid_codes:
+                    if possible_codes.get(line[-2], -1) == -1:
+                        possible_codes[line[-2]] = 1
+
+                    else:
+                        possible_codes[line[-2]] += 1
+
+            except IndexError:
+                pass
+
+        status_codes(size, possible_codes)
+
+    except KeyboardInterrupt:
+        status_codes(size, possible_codes)
+        raise
