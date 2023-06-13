97686656951613947258be5897ecfb9a819d4f1b
samuelselasi <samelsekasi@gmail.com>
2023-06-13 18:25:42 +0000

    Add script that imports an array and computes a new array

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// A script that imports an array and computes a new array
+
+const list = require('./100-data.js').list;
+const newList = list.map((value, index) => value * index);
+console.log(list + '\n' + newList);
