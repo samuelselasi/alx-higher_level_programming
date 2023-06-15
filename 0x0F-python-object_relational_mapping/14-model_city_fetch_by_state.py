1aa56a120bf1bd1c12f2d049b5fe7bd56f1764b5
samuelselasi <samelsekasi@gmail.com>
2023-06-15 21:59:06 +0000

    Add a script 14-model_city_fetch_by_state.py that prints all City objects from the database hbtn_0e_14_usa:

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""A script that prints all City objects from the database hbtn_0e_14_usa"""
+
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from model_state import State
+from model_city import City
+from sys import argv
+
+if __name__ == '__main__':
+    """Function that connects to MySQL server on localhost at port 3306"""
+
+    db = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(argv[1], argv[2],
+                                                          argv[3])
+    engine = create_engine(db)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    query = session.query(State.name, City.id, City.name).\
+        join(City, State.id == City.state_id).order_by(City.id)
+    result = query.all()
+    for row in result:
+        print("{:s}: ({:d}) {:s}".format(*row))
+    session.commit()
+    session.close()
