87f0764374bcb42f16b705e0a72ee9b0d91e0b5f
samuelselasi <samelsekasi@gmail.com>
2023-06-15 22:08:36 +0000

    Add a script 14-model_city_fetch_by_state.py that prints all City objects from the database hbtn_0e_14_usa

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -3,7 +3,7 @@
 
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-from model_state import State
+from model_state import Base, State
 from model_city import City
 from sys import argv
 
