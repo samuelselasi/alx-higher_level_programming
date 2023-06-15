2805778dddc3bb17a64ced7d181975b7b937d73d
samuelselasi <samelsekasi@gmail.com>
2023-06-15 22:13:56 +0000

    Add modified City class

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+A python file that contains class definition of City
+and instance Base = declarative_base()
+"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
+
+
+class City(Base):
+    """A class that defines City insatnces"""
+
+    __tablename__ = 'cities'
+    id = Column(Integer, autoincrement=True, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
