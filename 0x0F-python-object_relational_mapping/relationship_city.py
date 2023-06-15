ed8b26e22213c7076647214fac3d3aa3e720952c
samuelselasi <samelsekasi@gmail.com>
2023-06-15 22:44:32 +0000

    Fix City class

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -5,7 +5,7 @@ and instance Base = declarative_base()
 """
 
 from sqlalchemy import Column, Integer, String, ForeignKey
-from model_state import Base
+from relationship_state import Base
 
 
 class City(Base):
