e709a518c625041a8e718428578ecdd721c2244f
samuelselasi <samelsekasi@gmail.com>
2023-06-15 23:03:15 +0000

    Add a script that lists all State objects, and corresponding City objects, contained in the database hbtn_0e_101_usa

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+A script that creates the State California with the City San Francisco
+from the database hbtn_0e_100_usa
+"""
+
+from relationship_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from relationship_city import City
+from sys import argv
+
+if __name__ == "__main__":
+    """Function that connects to MySQL server on localhost at port 3306"""
+
+    db = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(argv[1], argv[2],
+                                                          argv[3])
+    engine = create_engine(db)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    query = session.query(State).order_by(State.id)
+    result = query.all()
+    for state in result:
+        print("{:d}: {:s}".format(state.id, state.name))
+        for cities in state.cities:
+            print("\t{:d}: {:s}".format(cities.id, cities.name))
+    session.commit()
+    session.close()
