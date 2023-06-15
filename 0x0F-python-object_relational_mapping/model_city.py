5fa083f6725e1bd8acf52f96e8bfc12fc681c70b
samuelselasi <samelsekasi@gmail.com>
2023-06-15 21:58:20 +0000

    Fix file that contains City class

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -11,7 +11,7 @@ from model_state import Base
 class City(Base):
     """A class that defines City insatnces"""
 
-    __tablename__ = 'states'
+    __tablename__ = 'cities'
     id = Column(Integer, autoincrement=True, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
