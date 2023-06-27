b8f58e450ea1675b06a84b75e8cb66999995b63d
samuelselasi <samelsekasi@gmail.com>
2023-06-27 21:45:12 +0000

    Add a Bash script that makes a request that causes the server to respond with a message containing You got me, in the body of the response.

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that makes reuest that causes the server to respond with message
-curl -s -X PUT -H "Content-Length: 0" -H "X-HolbertonSchool-User-Id: 98" -H "Origin: HolbertonSchool" -L "0.0.0.0:5000/catch_me" -o /dev/null -w "You got me!"
+curl -s -L -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
