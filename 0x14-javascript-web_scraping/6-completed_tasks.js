9338acb97e3b663fea10d2b75b8b55532e2fa8b7
samuelselasi <samelsekasi@gmail.com>
2023-07-25 23:29:55 +0000

    Test script that computes the number of tasks completed by user id

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,23 +1,46 @@
 #!/usr/bin/node
 
-const URL = process.argv[2];
 const request = require('request');
+const url = process.argv[2];
 
-request(URL, function (error, response, body) {
+request.get(url, { json: true }, (error, response, body) => {
   if (error) {
     console.log(error);
-  } else {
-    const completedTasks = JSON.parse(body);
-    const completed = {};
+    return;
+  }
 
-    for (const task of completedTasks) {
-      if (task.completed === true) {
-        if (task.completed && completed[task.userId] === undefined) {
-          completed[task.userId] = 0;
-        }
-        completed[task.userId] += 1;
+  if (response.statusCode !== 200) {
+    return;
+  }
+
+  const data = {};
+  body.forEach((todo) => {
+    if (todo.completed) {
+      if (!data[todo.userId]) {
+        data[todo.userId] = 1;
+      } else {
+        data[todo.userId]++;
       }
     }
-    console.log(completed);
+  });
+  let index = 0;
+  const length = Object.keys(data).length;
+  if (length === 0) {
+    console.log('{}');
+    return;
+  }
+  for (const key in data) {
+    if (index === 0) {
+      if (length !== 1) {
+        console.log('{ \'' + key + '\': ' + data[key] + ',');
+      } else {
+        console.log('{ \'' + key + '\': ' + data[key] + ' }');
+      }
+    } else if (index === length - 1) {
+      console.log('  \'' + key + '\': ' + data[key] + ' }');
+    } else {
+      console.log(`  '${key}': ${data[key]},`);
+    }
+    index++;
   }
 });
