e696abfeb79dd542f77ed23bca17230da0904b7c
samuelselasi <samelsekasi@gmail.com>
2023-07-25 11:42:32 +0000

    Add a script that computes the number of tasks completed by user id.

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const URL = process.argv[2];
+const request = require('request');
+
+request.get(URL, { json: true }, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  const completedTasks = {};
+  body.forEach((todo) => {
+    if (todo.completed) {
+      if (!completedTasks[todo.userId]) {
+        completedTasks[todo.userId] = 1;
+      } else {
+        completedTasks[todo.userId] += 1;
+      }
+    }
+  });
+  console.log(completedTasks);
+});
