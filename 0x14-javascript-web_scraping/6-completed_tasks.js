e70ceae25f86101939e2630b3a2de69d54203820
samuelselasi <samelsekasi@gmail.com>
2023-07-25 18:20:26 +0000

    Test script that computes the number of tasks completed by user id

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,19 +3,19 @@
 const URL = process.argv[2];
 const request = require('request');
 
-request.get(URL, (error, response, body) => {
+request(URL, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
     const completedTasks = JSON.parse(body);
     const dict = {};
 
-    for (const index of completedTasks) {
-      if (index.completed === true) {
-        if (dict[index.userId] === undefined) {
-          dict[index.userId] = 0;
+    for (const task of completedTasks) {
+      if (task.completed === true) {
+        if (dict[task.userId] === undefined) {
+          dict[task.userId] = 0;
         }
-        dict[index.userId] += 1;
+        dict[task.userId] += 1;
       }
     }
     console.log(dict);
