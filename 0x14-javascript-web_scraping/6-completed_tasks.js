7e2ae5f4724f619316862f08c1a60b9f69abae9c
samuelselasi <samelsekasi@gmail.com>
2023-07-25 23:51:36 +0000

    Test script that computes the number of tasks completed by user id

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,72 +1,48 @@
 #!/usr/bin/node
 
-const https = require('https');
-
 const URL = process.argv[2];
+const request = require('request');
 
-function handleResponse (response) {
-  let data = '';
-  response.on('data', (chunk) => {
-    data += chunk;
-  });
+request.get(URL, { json: true }, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
 
-  response.on('end', () => {
-    if (response.statusCode !== 200) {
-      console.error('Request failed with status code:', response.statusCode);
-      return;
-    }
+  if (response.statusCode !== 200) {
+    return;
+  }
 
-    try {
-      const completedTasks = JSON.parse(data);
-      const todos = {};
-
-      completedTasks.forEach((task) => {
-        if (task.completed) {
-          if (!todos[task.userId]) {
-            todos[task.userId] = 1;
-          } else {
-            todos[task.userId]++;
-          }
-        }
-      });
-
-      const length = Object.keys(todos).length;
-      if (length === 0) {
-        console.log('{}');
+  const todos = {};
+  body.forEach((todo) => {
+    if (todo.completed) {
+      if (!todos[todo.userId]) {
+        todos[todo.userId] = 1;
       } else {
-        console.log('{');
-        let task = 0;
-        for (const key in todos) {
-          if (task === length - 1) {
-            console.log(`  '${key}': ${todos[key]}`);
-          } else {
-            console.log(`  '${key}': ${todos[key]},`);
-          }
-          task++;
-        }
-        console.log('}');
+        todos[todo.userId]++;
       }
-    } catch (error) {
-      console.error('Error parsing JSON data:', error.message);
     }
   });
-}
 
-function handleRequestError (error) {
-  console.error('Error occurred while making the request:', error.message);
-}
+  let task = 0;
+  const length = Object.keys(todos).length;
+  if (length === 0) {
+    console.log('{}');
+    return;
+  }
 
-function makeRequest (url) {
-  const request = https.get(url, (response) => {
-    handleResponse(response);
-  });
-
-  request.on('error', handleRequestError);
-}
-
-if (!URL) {
-  console.error('Please provide a valid URL.');
-  process.exit(1);
-}
-
-makeRequest(URL);
+  for (const key in todos) {
+    if (task === 0) {
+      if (length !== 1) {
+        console.log('{ \'' + key + '\': ' + todos[key] + ',');
+      } else {
+        console.log('{ \'' + key + '\': ' + todos[key] + ' }');
+      }
+    } else if (task === length - 1) {
+      console.log('  \'' + key + '\': ' + todos[key] + ' }');
+    } else {
+      console.log(`  '${key}': ${todos[key]},`);
+    }
+    task++;
+  }
+});
