66a9b18c9de89ac22092b0a332061ace9adc3aa3
samuelselasi <samelsekasi@gmail.com>
2023-07-26 00:04:19 +0000

    Test script that computes the number of tasks completed by user id

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,30 +1,48 @@
 #!/usr/bin/node
 
-const request = require('request');
-
 const URL = process.argv[2];
+const request = require('request');
 
 request.get(URL, { json: true }, (error, response, body) => {
   if (error) {
-    console.error('Error occurred while making the request:', error.message);
+    console.log(error);
     return;
   }
 
   if (response.statusCode !== 200) {
-    console.error('Request failed with status code:', response.statusCode);
     return;
   }
 
-  const completedTasks = body.filter((task) => task.completed === true);
   const todos = {};
-
-  completedTasks.forEach((task) => {
-    if (!todos[task.userId]) {
-      todos[task.userId] = 1;
-    } else {
-      todos[task.userId]++;
+  body.forEach((todo) => {
+    if (todo.completed) {
+      if (!todos[todo.userId]) {
+        todos[todo.userId] = 1;
+      } else {
+        todos[todo.userId]++;
+      }
     }
   });
 
-  console.log(todos);
+  let task = 0;
+  const length = Object.keys(todos).length;
+  if (length === 0) {
+    console.log('{}');
+    return;
+  }
+
+  for (const key in todos) {
+    if (task === 0) {
+      if (length !== 1) {
+        console.log('{ \'' + key + '\': ' + todos[key] + ',');
+      } else {
+        console.log('{ \'' + key + '\': ' + todos[key] + ' }');
+      }
+    } else if (task === length - 1) {
+      console.log('  \'' + key + '\': ' + todos[key] + ' }');
+    } else {
+      console.log(`  '${key}': ${todos[key]},`);
+    }
+    task++;
+  }
 });
